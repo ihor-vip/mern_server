049fb60b1ad0e9000c6e5222c58ed79b2c727de6
ihor-vip <vip20140902@gmail.com>
2023-04-21 10:47:27 +0300

    Get user

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -111,8 +111,27 @@ export async function login(req,res){
 }
 
 export async function getUser(req,res){
-    res.json('getUser route')
+
+    const { username } = req.params;
+
+    try {
+
+        if(!username) return res.status(501).send({ error: "Invalid Username"});
+
+        UserModel.findOne({ username }, function(err, user){
+            if(err) return res.status(500).send({ err });
+            if(!user) return res.status(501).send({ error : "Couldn't Find the User"});
+
+            const { password, ...rest } = Object.assign({}, user.toJSON());
+
+            return res.status(201).send(rest);
+        })
+
+    } catch (error) {
+        return res.status(404).send({ error : "Cannot Find User Data"});
+    }
 }
+
 
 export async function updateUser(req,res){
   res.json('updateUser route')
