ef090a9f2b67dbbdab3739c16704ba5a63f81bb4
ihor-vip <vip20140902@gmail.com>
2023-04-21 08:40:32 +0300

    Create routes and controllers for backend

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,9 +1,3 @@
-import UserModel from '../model/User.model.js'
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import ENV from '../config.js'
-import otpGenerator from 'otp-generator';
-
 export async function register(req,res){
     res.json('register route')
 }
