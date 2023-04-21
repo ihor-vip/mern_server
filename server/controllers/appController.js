10b596de5f796b39331f0c2a4edb5ae35bf7c51a
ihor-vip <vip20140902@gmail.com>
2023-04-21 09:41:02 +0300

    Login endpoint and .env config file

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,5 +1,7 @@
 import UserModel from "../model/User.model.js";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import ENV from '../config.js'
 
 export async function register(req,res){
 
@@ -58,7 +60,41 @@ export async function register(req,res){
 
 
 export async function login(req,res){
-    res.json('login route')
+
+    const { username, password } = req.body;
+
+    try {
+
+        UserModel.findOne({ username })
+            .then(user => {
+                bcrypt.compare(password, user.password)
+                    .then(passwordCheck => {
+
+                        if(!passwordCheck) return res.status(400).send({ error: "Don't have Password"});
+
+                        const token = jwt.sign({
+                            userId: user._id,
+                            username : user.username
+                        }, ENV.JWT_SECRET , { expiresIn : "24h"});
+
+                        return res.status(200).send({
+                            msg: "Login Successful...!",
+                            username: user.username,
+                            token
+                        });
+
+                    })
+                    .catch(error =>{
+                        return res.status(400).send({ error: "Password does not Match"})
+                    })
+            })
+            .catch( error => {
+                return res.status(404).send({ error : "Username not Found"});
+            })
+
+    } catch (error) {
+        return res.status(500).send({ error});
+    }
 }
 
 export async function getUser(req,res){
